b0185bbdb47ebbdbae3f5123049e0964fe251da1
Andrea Pignanelli <andrea.pignanelli01@gmail.com>
2017-05-04 13:20:54 +0100

    fixed scoring

--- GoogleHash/fitness.py
+++ GoogleHash/fitness.py
@@ -11,49 +11,83 @@ def scoring(solution,data):
     total_requests=0
     gain = 0
     score = 0
-    l=0
-
-    cache_contents={}
-    for file in data["video_ed_request"]:
-        for ep in data["video_ed_request"][file]:
-            number_of_requests = data["video_ed_request"][file][ep][0]
-            connected_endpoints = data["ed_cache_list"][ep]
+    for video in data["video_ed_request"]:
+        for ep in data["video_ed_request"][video]:
+            number_of_requests = data["video_ed_request"][video][ep][0]
             datacenter_latency = data["ep_to_dc_latency"][ep]
-            caches=[datacenter_latency,]
+            connected_endpoints =  data["ed_cache_list"][ep]
+            caches = [datacenter_latency,]
             for cache in connected_endpoints:
-
-                if solution[cache][file] == 1:
-                    l += 1
+                if solution[cache][video] == 1:
                     caches.append(data["ep_to_cache_latency"][ep][cache])
 
-            min_latency= data["video_ed_request"][file][ep][1]
-            min_cache= min(caches)
-            if min_cache < min_latency:
-                data["video_ed_request"][file][ep][1] = min_cache
+            smallest_latency = data["video_ed_request"][video][ep][1]
+            min_cache = min(caches)
+            if min_cache < smallest_latency:
+                data["video_ed_request"][video][ep][1] = min_cache
 
             total_requests += number_of_requests
-            gain =(number_of_requests*(datacenter_latency-min_cache))
-            score = (gain*1000)
+            gain += (number_of_requests*(datacenter_latency-min_cache))
+    score = (gain*1000)
 
-    return score,caches,gain,min_cache,connected_endpoints,l
+    return score
 
 def move_validity(sol, cache, video, cache_contents, data):
     if sol[cache][video] == 1:
         return False
     return (cache_contents[cache] + data["video_size_desc"][video] <= data["cache_size"])
 
-# def hill_climber(solution,data,cache_in):
-#     valid_move=True
-#
-#     while valid_move:
-#         valid_move = False
-#         best_score = 0
-#         best_move = []
-#
-#         for row in range(len(solution)):
-#             for column in range (len(solution[row])):
-#                 if move_validity(solution, row, column, cache_in, data):
-#
+
+def change_score(cache,video,data):
+    gain=0
+    for ep in data["video_ed_request"][video]:
+        number_of_requests = data["video_ed_request"][video][ep][0]
+        connected_endpoints = data["ed_cache_list"][ep]
+
+    if cache in connected_endpoints:
+        smallest = data["video_ed_request"][video][ep][1]
+        cache_latency = data["ep_to_cache_latency"][ep][cache]
+
+        if cache_latency < smallest:
+            gain+=(number_of_requests*(smallest-cache_latency))
+
+    return gain
+
+
+def hill_climb(solution,data,cache_in):
+    valid_move=True
+
+    while valid_move:
+        valid_move = False
+        best_score = 0
+        best_move = []
+
+        for row in range(len(solution)):
+            for column in range (len(solution[row])):
+                if move_validity(solution, row, column, cache_in, data):
+                    currentscore = change_score(row,column,data)
+                    if currentscore > best_score:
+                        best_score =currentscore
+                        best_move=[row,column]
+                        move_found= True
+                    elif currentscore == best_score and currentscore > 0:
+                        print("test")
+
+
+        if move_found:
+            print("So far the best move is", best_move)
+            print("The best score is:", best_score)
+            cache, file = best_move[0], best_move[1]
+            solution[cache][file] = 1
+            cache_in[cache] += data["video_size_desc"][file]
+        else:
+            print("There are no better options")
+
+
+
+
+
+
 
 
 
@@ -63,11 +97,14 @@ def move_validity(sol, cache, video, cache_contents, data):
 
 
 data = read_google("input/test.in")
-#solution=[[0 for i in range(data["number_of_videos"])] for j in range(data["number_of_caches"])]
+# #solution=[[0 for i in range(data["number_of_videos"])] for j in range(data["number_of_caches"])]
 solution = [[0, 0, 1, 0, 0],[0, 1, 0, 1, 0],[1, 1, 0, 0, 0]]
-print("test solution:",solution)
-print(constraints(solution))
+# print(data["ep_to_cache_latency"])
+# print("test solution:",solution)
+# print(constraints(solution))
 if constraints(solution):
     print("final score",scoring(solution,data))
 else:
     print("nope")
+
+
